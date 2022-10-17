156eca2d233999a4c2bb95ae91e55bbcd223c6e8
Mike <king06618@gmail.com>
2022-10-17 20:06:05 +0200

    done

--- src/Main.java
+++ src/Main.java
@@ -8,66 +8,92 @@ public class Main {
     public static void main(String[] args) {
         String result, filename;
         int team;
-        String[] teams = new String[0];
+        String[] teams;
+        int[] points;
         List<String> es = new ArrayList<>();
+        List<String> teamslist = new ArrayList<>();
         List<String> totalGames = new ArrayList<>();
+        LinkedHashMap<String, Integer> scoresMap = new LinkedHashMap<>();
         Scanner sc = new Scanner(System.in);
         System.out.println("please enter the number from the following options below");
         System.out.println("1. Enter the Values Manually");
         System.out.println("2. File to read from");
 
         result = sc.nextLine();
-        if (result.equals("1"))
-        {
+        if (result.equals("1")) {
             try {
                 BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
-                System.out.print("Input number of teams:\t");
-                team = Integer.parseInt(input.readLine());
-                String[] textteams = new String[team];
-               for (int i = 0; i < team; i++) {
-                   System.out.println("please enter values please");
-                   textteams[i] = input.readLine();
+                System.out.print("Input number of Games:\t");
+                int games = Integer.parseInt(input.readLine());
+                team = games * 2;
+                for (int i = 0; i < games; i++) {
+                    System.out.println("please enter values please");
+                    result = input.readLine();
+                    String[] arrOfStr = result.split(",");
+                    Collections.addAll(es, arrOfStr);
                 }
-
-                teams =  new String[team];
 
                 System.out.println("-------------------------------------------------");
 
                 int[][] scoreBoard = new int[team][5];
-                for (int i = 0; i < team - 1; i++) {
-                    for (int j = i + 1; j < team; j++) {
-                        result = textteams[i];
-                        es = new ArrayList<String>(Arrays.asList(result.split(",")));
-                        int a = es.get(i).length();
-                        String teamnamea = es.get(i).substring(0 ,a-2);
-                        int teamAScore = Integer.parseInt(es.get(i).substring(a-1));
-                        int b = es.get(j).length();
-                        String teamnameb = es.get(j).substring(0 ,b-2);
-                        int teamBScore = Integer.parseInt(es.get(j).substring(b-1));
+                for (int i = 0; i < games - 1; i += 2) {
+                    int j = i + 1;
 
-                        scoreBoard[i][0]++;
-                        scoreBoard[j][0]++;
-                        if (teamAScore > teamBScore) {
-                            scoreBoard[i][1]++;
-                            scoreBoard[j][3]++;
-                            scoreBoard[i][4] += 3;
-                        } else if (teamAScore == teamBScore) {
-                            scoreBoard[i][2]++;
-                            scoreBoard[j][2]++;
-                            scoreBoard[i][4]++;
-                            scoreBoard[j][4]++;
-                        } else {
-                            scoreBoard[i][3]++;
-                            scoreBoard[j][1]++;
-                            scoreBoard[j][4] += 3;
-                        }
+                    int a = es.get(i).length();
+                    String teamnamea = es.get(i).substring(0, a - 2);
+                    int teamAScore = Integer.parseInt(es.get(i).substring(a - 1));
+                    int b = es.get(j).length();
+                    String teamnameb = es.get(j).substring(0, b - 2);
+                    int teamBScore = Integer.parseInt(es.get(j).substring(b - 1));
+
+                    scoreBoard[i][0]++;
+                    scoreBoard[j][0]++;
+                    if (teamAScore > teamBScore) {
+                        scoreBoard[i][1]++;
+                        scoreBoard[j][3]++;
+                        teamslist.add(teamnamea + " " + String.valueOf(scoreBoard[i][4] += 3));
+                        teamslist.add(teamnameb + " " + String.valueOf(scoreBoard[j][4] += 0));
+                    } else if (teamAScore == teamBScore) {
+                        scoreBoard[i][2]++;
+                        scoreBoard[j][2]++;
+                        teamslist.add(teamnamea + " " + String.valueOf(scoreBoard[i][4] += 1));
+                        teamslist.add(teamnameb + " " + String.valueOf(scoreBoard[j][4] += 1));
+                    } else {
+                        scoreBoard[i][3]++;
+                        scoreBoard[j][1]++;
+                        teamslist.add(teamnameb + " " + String.valueOf(scoreBoard[j][4] += 3));
+                        teamslist.add(teamnamea + " " + String.valueOf(scoreBoard[i][4] += 0));
                     }
                 }
+                for (int i = 0; i < team - 1; i++) {
+                    int a = teamslist.get(i).length();
+                    String teamName = teamslist.get(i).substring(0, a - 2);
+                    int amount = Integer.parseInt(teamslist.get(i).substring(a - 1));
+                    if (scoresMap.containsKey(teamName)) {
+                        scoresMap.put(teamName, scoresMap.get(teamName) + amount);
+                    } else {
+                        scoresMap.put(teamName, amount);
+                    }
+                }
+                int i = 1;
+                LinkedHashMap<String, Integer> sortedscoresMap = new LinkedHashMap<>();
+
+                scoresMap.entrySet()
+                        .stream()
+                        .sorted(Map.Entry.comparingByValue(Comparator.reverseOrder()))
+                        .forEachOrdered(x -> sortedscoresMap.put(x.getKey(), x.getValue()));
 
                 System.out.println("TEAMS POINTS");
-                for (int i = 0; i < team; i++) {
-                    System.out.println(teams[i] + scoreBoard[i][4]);
-                    es.clear();
+                int prev = -10;
+                for (Map.Entry<String, Integer> entry : sortedscoresMap.entrySet()) {
+                    String key = entry.getKey();
+                    Integer value = entry.getValue();
+                    if(prev == value) {
+                        i--;
+                    }
+                    System.out.println(String.valueOf(i) + " " + key + "  " + value + "pts");
+                    i++;
+                    prev = value;
                 }
             } catch (IOException e) {
                 System.out.println("Error");
@@ -95,58 +121,72 @@ public class Main {
             }
             System.out.println("-------------------------------------------------");
             if (!es.isEmpty()) {
-                team = totalGames.size();
-                teams =  new String[team];
+                team = es.size();
+                teams = new String[team];
+                points = new int[team];
                 int[][] scoreBoard = new int[team][5];
-                for (int i = 0; i < team - 1; i++) {
-                    for (int j = i + 1; j < team; j++) {
+                for (int i = 0; i < team - 1; i += 2) {
+                    int j = i + 1;
 
-                        int a = es.get(i).length();
-                        String teamnamea = es.get(i).substring(0 ,a-2);
-                        teams[i] = teamnamea;
-                        int teamAScore = Integer.parseInt(es.get(i).substring(a-1));
-                        int b = es.get(j).length();
-                        String teamnameb = es.get(j).substring(0 ,b-2);
-                        teams[j] = teamnameb;
-                        int teamBScore = Integer.parseInt(es.get(j).substring(b-1));
+                    int a = es.get(i).length();
+                    String teamnamea = es.get(i).substring(0, a - 2);
+                    teams[i] = teamnamea;
+                    int teamAScore = Integer.parseInt(es.get(i).substring(a - 1));
+                    int b = es.get(j).length();
+                    String teamnameb = es.get(j).substring(0, b - 2);
+                    teams[j] = teamnameb;
+                    int teamBScore = Integer.parseInt(es.get(j).substring(b - 1));
 
-                        scoreBoard[i][0]++;
-                        scoreBoard[j][0]++;
-                        if (teamAScore > teamBScore) {
-                            scoreBoard[i][1]++;
-                            scoreBoard[j][3]++;
-                            scoreBoard[i][4] += 3;
-                        } else if (teamAScore == teamBScore) {
-                            scoreBoard[i][2]++;
-                            scoreBoard[j][2]++;
-                            scoreBoard[i][4]++;
-                            scoreBoard[j][4]++;
-                        } else {
-                            scoreBoard[i][3]++;
-                            scoreBoard[j][1]++;
-                            scoreBoard[j][4] += 3;
-                        }
+                    scoreBoard[i][0]++;
+                    scoreBoard[j][0]++;
+                    if (teamAScore > teamBScore) {
+                        scoreBoard[i][1]++;
+                        scoreBoard[j][3]++;
+                        teamslist.add(teamnamea + " " + String.valueOf(scoreBoard[i][4] += 3));
+                        teamslist.add(teamnameb + " " + String.valueOf(scoreBoard[j][4] += 0));
+                    } else if (teamAScore == teamBScore) {
+                        scoreBoard[i][2]++;
+                        scoreBoard[j][2]++;
+                        teamslist.add(teamnamea + " " + String.valueOf(scoreBoard[i][4] += 1));
+                        teamslist.add(teamnameb + " " + String.valueOf(scoreBoard[j][4] += 1));
+                    } else {
+                        scoreBoard[i][3]++;
+                        scoreBoard[j][1]++;
+                        teamslist.add(teamnameb + " " + String.valueOf(scoreBoard[j][4] += 3));
+                        teamslist.add(teamnamea + " " + String.valueOf(scoreBoard[i][4] += 0));
                     }
                 }
+                for (int i = 0; i < team; i++) {
+                    int a = teamslist.get(i).length();
+                    String teamName = teamslist.get(i).substring(0, a - 2);
+                    int amount = Integer.parseInt(teamslist.get(i).substring(a - 1));
+                    if (scoresMap.containsKey(teamName)) {
+                        scoresMap.put(teamName, scoresMap.get(teamName) + amount);
+                    } else {
+                        scoresMap.put(teamName, amount);
+                    }
+                }
+                int i = 1;
+                LinkedHashMap<String, Integer> sortedscoresMap = new LinkedHashMap<>();
+
+                scoresMap.entrySet()
+                        .stream()
+                        .sorted(Map.Entry.comparingByValue(Comparator.reverseOrder()))
+                        .forEachOrdered(x -> sortedscoresMap.put(x.getKey(), x.getValue()));
 
                 System.out.println("TEAMS POINTS");
-                for (int i = 0; i < team; i++) {
-                    System.out.println(teams[i] + scoreBoard[i][4]);
-                    es.clear();
+                int prev = -10;
+                for (Map.Entry<String, Integer> entry : sortedscoresMap.entrySet()) {
+                    String key = entry.getKey();
+                    Integer value = entry.getValue();
+                    if(prev == value) {
+                        i--;
+                    }
+                    System.out.println(String.valueOf(i) + " " + key + "  " + value + "pts");
+                    i++;
+                    prev = value;
                 }
             }
         }
-    }
-    public static void removeDuplicates(String[] a)
-    {
-        LinkedHashSet<String> set
-                = new LinkedHashSet<String>();
-
-        // adding elements to LinkedHashSet
-        for (int i = 0; i < a.length; i++)
-            set.add(a[i]);
-
-        // Print the elements of LinkedHashSet
-        System.out.print(set);
     }
 }
